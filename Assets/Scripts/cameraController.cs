c06b3c0222432a40543f63685a71300550a87d1d
Rafael <rpclerici@gmail.com>
2021-11-09 23:36:30 -0300

    First steps
    Player movement and scenario added, including the four rooms, three corridors and eight obstacles.

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public Transform playerPosition;
+
+    public float Sensitivity = 200f;
+    public float visionAngle = 90f;
+    [Range(0,90)]
+
+    private float headCameraAngle = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * Sensitivity * Time.deltaTime;
+        float SensitivityCorrection = Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime;
+
+        if(SensitivityCorrection > 0f){
+            Sensitivity = Sensitivity + 50f;
+        }
+        else if(SensitivityCorrection < 0f){
+            Sensitivity = Sensitivity - 50f;
+        }
+
+        if(Sensitivity <= 1f){
+            Sensitivity = 25f;
+        }
+
+        headCameraAngle -= mouseY;
+        headCameraAngle = Mathf.Clamp(headCameraAngle, -visionAngle, visionAngle);
+
+        transform.localRotation = Quaternion.Euler(headCameraAngle, 0f, 0f);
+
+        playerPosition.Rotate(Vector3.up * mouseX);
+
+    }
+
+}
---
+++ Assets/Scripts/movementController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementController : MonoBehaviour
+{
+    public float Speed = 5f;
+    public CharacterController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        //Make sure character won't move faster in diagonal
+        movementInput.Normalize();
+
+        Vector3 movementController = movementInput.y * transform.forward + movementInput.x * transform.right;
+
+        // Ability to "run" pressing left shift (aka Fire3)
+        if(Input.GetAxis("Fire3") != 0){
+            movementController = movementController * 2;
+        }
+        playerController.Move(movementController * Speed * Time.deltaTime);
+    }
+}
